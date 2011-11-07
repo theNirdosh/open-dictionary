3a06ca183d2b680dc58bdbd13b37bfd549c68dd4
fen_li73@yahoo.com <fen_li73@yahoo.com@866fffed-f7ed-23c2-349e-ce3b1e89f7a6>
2011-11-07 20:15:08 +0000

    testing

---
+++ src/com/fenlisproject/opendictionary/OpenDictionaryActivity.java
@@ -0,0 +1,13 @@
+package com.fenlisproject.opendictionary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenDictionaryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
